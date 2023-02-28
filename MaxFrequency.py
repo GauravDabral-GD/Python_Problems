72c1ac2211af6fb3820818b60fc1cc6bc5f7f76b
Gaurav <gauravdabral201@gmail.com>
2023-02-28 21:46:27 +0530

    Max frequency

---
+++ MaxFrequency.py
@@ -0,0 +1,26 @@
+a = (input("Enter a sequence of numbers: ")).split(' ')
+
+d = {}
+
+def maxfreq(a):
+    '''This function checks for the number in a sequence occuring with maximum frequency. '''
+
+    for item in a:
+        if item not in d:
+            d[item] = 1
+        else:
+            d[item] += 1
+        
+    maxVal = max(d.values())
+    maxKey = 0
+    for key in d.keys():
+        if d[key] == maxVal:
+            maxKey = key
+    
+    return (f'{maxKey} has maximum frequency, it appears {maxVal} times in the sequence.')
+
+print(maxfreq(a))
+
+            
+
+
